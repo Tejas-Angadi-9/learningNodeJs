abef84f5768e7f47e939ef8f3472c07abcb1a360
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2024-02-26 13:11:11 +0530

    Learned the post method of req body

--- section_6_express_js/index.js
+++ section_6_express_js/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.use(express.json());
@@ -6,31 +7,82 @@ PORT = 4000;
 
 // ------------- STARTING THE SERVER --------------
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+    console.log(`Server running at port ${PORT}...`);
 })
  
 // ----------------- ROUTES -----------------------
 // Get method
+// app.get('/', (req, res) => {
+//     res.json([{username: 'Tejasangadi9', name: 'Tejas Angadi'}, {username: 'Tejasangadi9', name: 'Tejas Angadi'}]).status(200);
+// })
+
+// // Post method
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// })
+
+// // Put method
+// app.put('/', (req, res) => {
+//     res.send('You can put to this endpoint....');
+// })
+
+// // Patch method
+// app.patch('/', (req, res) => {
+//     res.send('You can patch to this endpoint....');
+// })
+
+// // Delete method
+// app.delete('/', (req, res) => {
+//     res.send('You can delete in this endpoint....');
+// })
+
+// APP - NATOURS 
 app.get('/', (req, res) => {
-    res.json([{username: 'Tejasangadi9', name: 'Tejas Angadi'}, {username: 'Tejasangadi9', name: 'Tejas Angadi'}]).status(200);
+    res.send('This is the Natours home page');
 })
 
-// Post method
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+const tours = require('./dev-data/data/tours-simple.json');
+
+// const tours = fs.readFile('./dev-data/data/tours-simple.json',)
+
+
+app.get('/api/v1/tours', (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
-// Put method
-app.put('/', (req, res) => {
-    res.send('You can put to this endpoint....');
-})
+app.post('/api/v1/tours', (req, res) => {
+    // const {name, duration, difficulty} = req.body;
+    // if(!name || !duration || !difficulty){
+    //     return res
+    //             .status(400)
+    //             .send('Fill all the fields')
+    // }
 
-// Patch method
-app.patch('/', (req, res) => {
-    res.send('You can patch to this endpoint....');
-})
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
 
-// Delete method
-app.delete('/', (req, res) => {
-    res.send('You can delete in this endpoint....');
+    // writting the updated tours
+    fs.writeFile('section_6_express_js/dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+        res.status(201).json({
+           status: "success",
+           data: {
+            tour: newTour
+           } 
+        })
+    })
+
+    res
+     .status(201)
+     .send('Created a new tour')
+    
+    console.log(req.body)
+
 })
