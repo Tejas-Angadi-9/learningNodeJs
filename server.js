675750e93a36fe1e5193b9a4a9d9453a0ca17195
Tejas Angadi <tejasangadi456@gmail.com>
2024-03-03 00:20:42 +0530

    Connected the Cloud DB with server using mongoose

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const app = require('./index')
 const dotenv = require('dotenv')
-
+const mongoose = require('mongoose')
 
 // dotenv.config({ path: `./config.env` })
 console.log(app.get('env'))
 
 dotenv.config();
-console.log(process.env.MY_USERNAME)
+console.log(process.env.PORT)
+
+mongoose.connect(process.env.DATABASE)
+    .then(() => console.log('Connected to database successfully!')) // this should be have a callback function (COMPULSORY)
+    .catch((err) => {
+        console.log('Failed to connect with DB');
+        console.error(err.message);
+        process.exit(1)
+    })
 
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
