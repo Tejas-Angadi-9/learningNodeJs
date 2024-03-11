97472bb97170b80c7b49f1bdfafe2463d3625c6c
Tejas Angadi <tejasangadi456@gmail.com>
2024-03-11 10:00:18 +0530

    Added the pagination feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -69,6 +69,20 @@ exports.getAllTours = async (req, res) => {
             query = query.select('-__v')
         }
 
+        //! 4) Pagination
+        // ? page=2&limit=10, This means we want page 2 and limit 10. 1-10 data on page1, 11-20 on page2, 21-30 on page3
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numTours = await tourModel.countDocuments();
+            console.log("Total docs: ", numTours)
+            if (skip >= numTours)
+                throw new Error('This page does not exist')
+        }
+
         //! EXECUTE QUERY
         const tours = await query;
 
